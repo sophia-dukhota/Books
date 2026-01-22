a58ce06c1fbd784f9a152c5b95919dbdfefe5150
Sophia Dukhota <ptl93047@openwork.uk.com>
2026-01-22 21:16:58 +0000

    Add project files.

---
+++ Books.Shared/Models/Books.cs
@@ -0,0 +1,9 @@
+﻿namespace Books.Shared.Models
+{
+    public class Books
+    {
+        public string? Name { get; set; }
+        public int chapter { get; set; }
+        public string comment { get; set; }
+    }
+}

e8c13795f377f2f89fd19c707c26f627da75f5f7
Sophia Dukhota <ptl93047@openwork.uk.com>
2026-01-23 16:41:03 +0000

    added in api url however commit because need to undo problems with dockerfile

--- Books.Frontend/Program.cs
+++ Books.Frontend/Program.cs
@@ -11,6 +11,8 @@ namespace Books.Frontend
             builder.RootComponents.Add<App>("#app");
             builder.RootComponents.Add<HeadOutlet>("head::after");
 
+            builder.Services.AddHttpClient();
+
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
             await builder.Build().RunAsync();
